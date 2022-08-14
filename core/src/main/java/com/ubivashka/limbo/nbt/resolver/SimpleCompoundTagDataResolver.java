940f6d64521ffb587a097d92e42532a00662b55b
U61vashka <botyrbojey@gmail.com>
2022-08-14 23:02:31 +0600

    Fix #1 (again)

--- core/src/main/java/com/ubivashka/limbo/nbt/resolver/SimpleCompoundTagDataResolver.java
+++ core/src/main/java/com/ubivashka/limbo/nbt/resolver/SimpleCompoundTagDataResolver.java
@@ -58,7 +58,7 @@ public class SimpleCompoundTagDataResolver extends AbstractCompoundTagDataResolv
                     collection = collection.stream().map(collectionObject -> SimpleTagFactory.getFactory(dataType.getId()).createTag(collectionObject, context)).collect(
                             Collectors.toList());
 
-                return new ListTag<>(dataType.getTagClass(), collection);
+                return new ListTag(dataType.getTagClass(), collection);
             }
             throw new IllegalStateException("Cannot resolve object " + object.getClass().getSimpleName() + ",field name " + context.getField().getName() + ", value " + object);
         });
